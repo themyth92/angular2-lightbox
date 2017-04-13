0fb0463f26b79941d096f27dbb4be1b2ac5e3051
paul <themyth92@gmail.com>
2017-04-13 18:03:19 +0800

    Update demo

--- systemjs.config.js
+++ systemjs.config.js
@@ -14,7 +14,6 @@
     // map tells the System loader where to look for things
     map: {
       main: 'demo',
-      lightbox: 'src/',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -23,6 +22,7 @@
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      lightbox: 'npm:angular2-lightbox/src',
       rxjs: 'npm:rxjs'
     },
 
@@ -31,11 +31,11 @@
         main: './main',
         defaultExtension: 'ts'
       },
-      lightbox: {
-        main: './index',
-        defaultExtension: 'ts'
-      },
       rxjs: {
+        defaultExtension: 'js'
+      },
+      lightbox: {
+        main: './index.js',
         defaultExtension: 'js'
       }
     }
